693c9b4a3f7d92a698e826d28a7f69a98f22276f
rpss2 <rpss@cin.ufpe.br>
2018-11-07 18:51:22 -0300

    update customer

--- Entrega3/librariumService/controllers/customerController.js
+++ Entrega3/librariumService/controllers/customerController.js
@@ -1,7 +1,5 @@
-const Customer = require('../models/customerModel')
-
+const customer = require('../models/customerModel')
 const customerErrors = require('../errors/customerErrors.json')
-
 const version = require('../package.json').version
 
 exports.createCustomer = async function(customerData, callback) {
@@ -14,7 +12,7 @@ exports.createCustomer = async function(customerData, callback) {
             customerData.creditCard
         ) {
             customerData.version = version
-            Customer.create(customerData, function(e, c) {
+            customer.create(customerData, function(e, c) {
                 if (e) {
                     callback.error(customerErrors.failedCreation)
                 } else {
@@ -30,11 +28,40 @@ exports.createCustomer = async function(customerData, callback) {
 }
 
 exports.getAll = async function(callback) {
-    Customer.find(function(err, customers) {
+    customer.find(function(err, customers) {
         if (err) {
             callback.error(customerErrors['noCustomerForId'])
         } else {
             callback.accept(customers)
         }
     })
+}
+
+exports.getProduct = async function(customerId, callback) {
+    customer.findById(customerId, function(err, customer) {
+        if (err || customer == null) {
+            callback.error(customerErrors['noCustomerForId'])
+        } else {
+            callback.accept(customer)
+        }
+    })
+}
+
+exports.updateProduct = async function(oldCustomer, newCustomer, callback) {
+    if (newCustomer) {
+        oldCustomer.email = newCustomer.email
+        oldCustomer.password = newCustomer.password
+        oldCustomer.name = newCustomer.name
+        oldCustomer.address = newCustomer.address
+        oldCustomer.creditCard = newCustomer.creditCard
+        oldCustomer.save(function(e) {
+            if (e) {
+                callback.error(customerErrors['failedUpdate'])
+            } else {
+                callback.accept(newCustomer)
+            }
+        })
+    } else {
+        callback.error(productErrors['badlyFormedProduct'])
+    }
 }
--- Entrega3/librariumService/routes/customer.js
+++ Entrega3/librariumService/routes/customer.js
@@ -1,3 +1,5 @@
+const Customer = require('../models/customerModel')
+
 app.post('/customers', (req, res) => {
     let customerData = req.body.customerData
     customerController.createCustomer(customerData, {
@@ -17,6 +19,26 @@ app.get('/customers', function(req, res) {
         },
         error(e) {
             res.status(e.httpStatus).json(e.data)
+        }
+    })
+})
+
+app.post('/customers/:customerId/update', (req, res) => {
+    let customerData = req.body.customerData
+    let customerId = req.params.customerId
+    Customer.findById(customerId, function(err, customer) {
+        if (err || customer == null) {
+            let e = productErrors['noProductForId']
+            res.status(e.httpStatus).json(e.data)
+        } else {
+            customerController.updateProduct(customer, customerData, {
+                accept(c) {
+                    res.status(201).json(c)
+                },
+                error(e) {
+                    res.status(e.httpStatus).json(e.data)
+                }
+            })
         }
     })
 })
